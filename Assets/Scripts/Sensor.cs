7c9f5370f8444489d658ac9e3137599430551ccf
dandreu7 <157640055+dandreu7@users.noreply.github.com>
2024-10-02 20:22:33 -0400

    Reactions Added

--- Assets/Scripts/Sensor.cs
+++ Assets/Scripts/Sensor.cs
@@ -6,50 +6,54 @@ public class SensorPlane : MonoBehaviour {
     public MeshRenderer displayRenderer;
     private List<string> itemsOnSensor = new List<string>();
 
-    // Dictionary to store possible reactions
+    // Reactions Dictionary
     private Dictionary<List<string>, Material> reactionResults = new Dictionary<List<string>, Material>();
 
     void Start() {
-        // Add possible item combinations and their corresponding result materials
+        // How to use:
+        // Add new reactions with the following format:
+        // reactionResults.Add(new List<string> {CHEMICALS FOR REACTION HERE}, Resources.Load<Material>(PATH TO MATERIAL));
+        // make sure that materials are under Assets/Resources/Materials/Reactions
         reactionResults.Add(new List<string> { "Baking Soda", "Vinegar" }, Resources.Load<Material>("Materials/Reactions/Volcano Material"));
-        // Add other reactions here
+        
     }
 
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Pickups")) {
-            string itemName = other.gameObject.name;
-            if (!itemsOnSensor.Contains(itemName) && itemsOnSensor.Count < 5) {
+            string itemName = other.gameObject.name; // Gets name of item
+            if (!itemsOnSensor.Contains(itemName) && itemsOnSensor.Count <= 5) { // Max of 5 items on the sensor
                 itemsOnSensor.Add(itemName);
                 CheckReaction();
-                LogItemsOnSensor(); // Log items when an item is added
+                LogItemsOnSensor(); // Log when an item is added
             }
         }
     }
 
     private void OnTriggerExit(Collider other) {
         if (other.CompareTag("Pickups")) {
-            string itemName = other.gameObject.name;
+            string itemName = other.gameObject.name; // Gets name of item
             if (itemsOnSensor.Contains(itemName)) {
                 itemsOnSensor.Remove(itemName);
-                displayRenderer.material = defaultMaterial; // Reset the display when items are removed
-                LogItemsOnSensor(); // Log items when an item is removed
+                displayRenderer.material = defaultMaterial; // Reset the display when an item is removed
+                LogItemsOnSensor(); // Log when an item is removed
             }
         }
     }
 
     private void CheckReaction() {
         foreach (var reaction in reactionResults) {
+            // Check through dictionary to see if any of the chemicals on the sensor add up to a reaction in the dictionary
             if (new HashSet<string>(reaction.Key).SetEquals(itemsOnSensor)) {
                 displayRenderer.material = reaction.Value;
                 return;
             }
         }
 
-        // Reset to default if no reaction found
+        // If no reaction found, set to default material
         displayRenderer.material = defaultMaterial;
     }
 
-    // Method to log the items currently on the sensor
+    // Debug Log items on sensor
     private void LogItemsOnSensor() {
         string items = "Items on sensor: " + string.Join(", ", itemsOnSensor);
         Debug.Log(items);
